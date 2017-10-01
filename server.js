d1393aa00be83d76e792528cb620ec123072349a
Kamlesh Kumar <kamlesh.kumar@jadeglobal.com>
2017-10-01 21:35:43 +0530

    Added project page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about',(req,res)=>{
   })
 });
 
+app.get('/projects',(req,res)=>{
+  // res.send('inside about page')
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  })
+});
+
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage:'Unable to handle request'
